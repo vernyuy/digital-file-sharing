5d361e8ca81d818cbf341175bc0830d63bb8d105
fonchu <fonchu.e.venyuy@gmail.com>
2024-11-20 06:48:44 +0100

    updates

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,27 +3,27 @@ import { defineBackend } from '@aws-amplify/backend';
 // import { data } from './data/resource';
 import { storage } from './storage/resource';
 import {paymentWebhook} from './funtions/paymentWebhook/resource'
-// import {FunctionUrlAuthType} from 'aws-cdk-lib/aws-lambda'
-// import {PolicyStatement } from 'aws-cdk-lib/aws-iam'
+import {FunctionUrlAuthType} from 'aws-cdk-lib/aws-lambda'
+import {PolicyStatement } from 'aws-cdk-lib/aws-iam'
 
-// const backend = 
-defineBackend({
+const backend =  defineBackend({
   storage,
   paymentWebhook,
 });
 
-// const funcURL = backend.paymentWebhook.resources.lambda.addFunctionUrl({
-//   authType: FunctionUrlAuthType.NONE
-// })
+const funcURL = backend.paymentWebhook.resources.lambda.addFunctionUrl({
+  authType: FunctionUrlAuthType.NONE
+})
 
-// backend.paymentWebhook.resources.lambda.addToRolePolicy(
-//   new PolicyStatement({
-//     actions: ['ses:sendemail', 'ses:SendRawEmail']
-//   })
-// )
+backend.paymentWebhook.resources.lambda.addToRolePolicy(
+  new PolicyStatement({
+    actions: ['ses:sendemail', 'ses:SendRawEmail'],
+    resources: ['*']
+  })
+)
 
-// backend.addOutput({
-//   custom: {
-//     paymentWebhookUrl: funcURL.url
-//   }
-// })
+backend.addOutput({
+  custom: {
+    paymentWebhookUrl: funcURL.url
+  }
+})
